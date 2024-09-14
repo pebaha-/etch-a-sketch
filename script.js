137d6e5c18e5f63d3b577658a5c0a087dec33d10
Peter Bartram Hansen <pebaha@gmail.com>
2024-09-14 21:11:01 +0200

    Add user input for custom grid size (works but missing square shape)

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const container = document.querySelector("#container");
-let gridSize = 16;
+drawGrid(16);
 
-for (i = 1; i <= gridSize * gridSize; i++) {
-    let div = document.createElement("div");
-    div.setAttribute("class", "grid-element")
-    div.addEventListener('mouseenter', (event) => {
-        div.classList.add("colored-div");
-    });
-    div.textContent = i;
-    container.appendChild(div);
+function drawGrid(gridSize) {
+    for (i = 1; i <= gridSize * gridSize; i++) {
+        let div = document.createElement("div");
+        div.setAttribute("class", "grid-element")
+        div.addEventListener('mouseenter', (event) => {
+            div.classList.add("colored-div");
+        });
+        div.textContent = i;
+        container.appendChild(div);
+    }
 }
+
+function clearGrid() {
+    // While the container has a child, remove the last child.
+    // We remove the last child as it's faster than removing the first child
+    // since avoid re-indexing all of the remaining children.
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function isInteger(value) {
+    if (parseInt(value, 10).toString() === value) {
+        return true
+    }
+    return false;
+}
+
+const button = document.querySelector("#inputBtn");
+button.addEventListener('click', (event) => {
+    let newSize = prompt("Please enter size for the grid (eg. 16)", 16);
+
+    if (newSize != null && isInteger(newSize)) {
+        newSize = Number(newSize);
+        if (newSize > 100) {
+            alert("Grid size is too big. Please enter a value no more than 100.")
+        } else {
+            clearGrid();
+            drawGrid(newSize);
+        }
+    }
+});
